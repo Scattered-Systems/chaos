1f2f9011ef6de3cde23b37491829aa2a701a5e8e
FL03 <jo3mccain@icloud.com>
2022-05-25 10:03:28 -0500

    Commit
    Signed-off-by: FL03 <jo3mccain@icloud.com>

---
+++ .space.kts
@@ -0,0 +1,14 @@
+job("Build and Push: Chaos") {
+    docker {
+        build {
+            context = "."
+            file = "./Dockerfile"
+            args["HTTP_PROXY"] = "http://0.0.0.0:8080"
+            labels["vendor"] = "scattered-systems"
+        }
+
+        push("scattered-systems.registry.jetbrains.space/p/scsys/containers/chaos") {
+            tags("0.0.\$JB_SPACE_EXECUTION_NUMBER", "latest")
+        }
+    }
+}
