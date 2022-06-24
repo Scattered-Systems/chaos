e2c895b58580118abc9e413336c57b53936ebad9
FL03 <jo3mccain@icloud.com>
2022-06-24 17:42:06 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- chaos/src/main.rs
+++ chaos/src/main.rs
@@ -1,27 +1,12 @@
 /*
-    Author: Joe McCain III
-    Email: jo3mccain@icloud.com
-    Date: June 812, 2022
-    Package: chaos
-    Project: Chaos
-    Version: 0.1.0
-
-    Overview:
-            Chaos is intended to be deployed at an Ethereum Name, scaffolding the framework for a
-        highly customizable private network to be created for the user. This also serves a number of
-        roles in the Scattered Ecosystem, enabling the gateway to leverage the full might of
-        cloud-based technologies while furthering the experience by incorporating a number of
-        userful IoT features.
-
-    Quickstart
-        Run the application in two terminals, if your computer allows for mDNS than the nodes
-        will automatically connect enabling you to input the commands below to store values on your
-        network
-        Commands
-            * GET <key> <value>
-            * GET_PROVIDERS <key>
-            * PUT <key>
-            * PUT_PROVIDER <key>
+    Appellation: Chaos
+    Context: binary
+    Creator: FL03 <jo3mccain@icloud.com>
+    Description:
+        Chaos is designed to implement the storage capabilities of the SCSYS Ecosystem.
+        Built leveraging libp2p and other custom support crates, Chaos results in a simple to use
+        blockchain optimized for the end-user experience without making traditional sacrifices in
+        terms of security.
  */
 pub(crate) use crate::{
     application::*,
