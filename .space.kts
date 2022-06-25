852bbe9e1cc7a7f5162d9b1af595c8e9d848e286
FL03 <jo3mccain@icloud.com>
2022-06-25 01:04:43 -0500

    Update
    Signed-off-by: FL03 <jo3mccain@icloud.com>

--- .space.kts
+++ .space.kts
@@ -15,7 +15,7 @@ job("Docker: Build and Push Chaos") {
             labels["vendor"] = "scattered-systems"
         }
 
-        push("scattered-systems.registry.jetbrains.space/p/scsys/containers/chaos") {
+        push("scattered-systems.registry.jetbrains.space/p/chaos/containers/chaos") {
             tags("0.0.\$JB_SPACE_EXECUTION_NUMBER", "latest")
         }
     }
